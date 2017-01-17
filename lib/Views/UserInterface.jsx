19b175caf2f772c770381849fa9a96db47e1cffd
Rebecca Dengate <rebecca.dengate@nicta.com.au>
2017-01-18 09:40:53 +1100

    moved help screens to TerriaJS

--- lib/Views/UserInterface.jsx
+++ lib/Views/UserInterface.jsx
@@ -4,10 +4,10 @@ import version from '../../version';
 
 import StandardUserInterface from 'terriajs/lib/ReactViews/StandardUserInterface/StandardUserInterface.jsx';
 import MenuItem from 'terriajs/lib/ReactViews/StandardUserInterface/customizable/MenuItem';
-import HelpPanel from './HelpPanel';
+import HelpPanel from 'terriajs/lib/ReactViews/HelpScreens/HelpPanel';
 import { Menu } from 'terriajs/lib/ReactViews/StandardUserInterface/customizable/Groups';
-import HelpScreenWindow from './HelpScreenWindow.jsx';
-import ObscureOverlay from './ObscureOverlay.jsx';
+import HelpScreenWindow from 'terriajs/lib/ReactViews/HelpScreens/HelpScreenWindow.jsx';
+import ObscureOverlay from 'terriajs/lib/ReactViews/HelpScreens/ObscureOverlay.jsx';
 
 import './global.scss';
 
